2e7fd070e7b841bf35a7b769b9d52ab2f490f7f2
RLARN <msjdoc0645@naver.com>
2021-09-29 11:31:58 +0900

    keyprops 추가

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 const foodILike =[
   {
+    id: 1,
     name: 'kimchi',
     Image: 'https://cdn.imweb.me/thumbnail/20200415/6b6e035658bac.png',
   },
   {
+    id: 2,
     name: 'bap',
-    Image: 'https://lh3.googleusercontent.com/proxy/YLoy2w3dwQpPfTp86wpvjZzaOIAofgN9sQTV5N3ZoVtBN2UmO5ra2IqrqDhBGlyUnbey9wqP2FHlRSPBozg36qA',
+    Image: 'https://lh3.googleusercontent.com/proxy/Ia5SM0H7Kq971udLTKP_-GG4wmjOWh11hZJlKrJ0UoGqeSRna3D8wQaA3hkFn0gDg0BlJ2FMcj5lxYlM_1Bn3y9j6Xxy-TsAYVwX58MpSC-Opnk3kiepi9RLiTVDRnKMcvE9740l',
   }
 ]
 
 
-function renderFood(foo){
-  return <Food name={foo.name} picture={foo.Image}/>
-}
 
 function App() {
   return (
     <div>
       {
-        foodILike.map(renderFood)
+        foodILike.map(dish => (
+          <Food key={dish.id} name={dish.name} picture={dish.Image} />
+        ))
       }
     </div>
   );
@@ -31,7 +32,7 @@ function Food({name,picture}){
   return(
     <div>
       <h2>i love {name}</h2>
-      <img src = {picture} />
+      <img src = {picture} alt={name}/>
     </div>
   );
 }
