46422ceba6f23e6a2f736ebd48eaeb9323b414a2
RLARN <msjdoc0645@naver.com>
2021-10-06 12:05:24 +0900

    this.getMovie 영화정보 불러오기

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
+import axios from 'axios';
 import React from 'react';
 
  class App extends React.Component{
     state = {
     isLoading: true,
+    movies : [],
  };
 
+
+ getMovie = () => {
+   const movie = axios.get("https://yts-proxy.now.sh/list_movies.json");
+ }
+
  componentDidMount(){
-   setTimeout(() => {
-     this.setState({isLoading: false});
-   }, 6000);
+  this.getMovie();
  }
 
 
