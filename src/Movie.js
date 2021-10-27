78be52abe635394838ac205ede759b6e6b5925a4
RLARN <msjdoc0645@naver.com>
2021-10-27 10:31:33 +0900

    key props 추가, css 코드 복사

--- src/Movie.js
+++ src/Movie.js
@@ -10,8 +10,8 @@ function Movie({ title, year, summary, poster, genres}){
                 <h3 className="movie__title">{title}</h3>
                 <h5 className="movie__year">{year}</h5>
                 <ul className="monie__genres">
-                    {genres.map((genre) => {
-                        return <li className="movie__genre">{genre}</li>
+                    {genres.map((genre, index) => {
+                        return <li key={index} className="movie__genre">{genre}</li>
                     })}
                     </ul>
                 <p className="movie__summary">{summary}</p>
