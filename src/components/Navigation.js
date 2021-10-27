0f19d11c27f39d9d5b9dec9e53c69050b0a36f82
RLARN <msjdoc0645@naver.com>
2021-10-27 12:25:31 +0900

    라우터 사용위해 폴더 및 파일 정리, 네비게이션 컴포넌트 삽입

---
+++ src/components/Navigation.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Navigation(){
+
+    return(
+        <div>
+            <a href="/">Home</a>
+            <a href="/about">About</a>
+        </div>
+    )
+}
+
+export default Navigation
---
+++ src/routes/About.js
@@ -0,0 +1,15 @@
+import React from "react";
+import './About.css'
+
+function About(){
+    return (
+    <div className="about__container">
+        <sapn>
+            About this page: I Build it becasuse it becasuse I love movies.
+        </sapn>
+        <span>- George Orwell, 1984</span>
+    </div>
+    )
+}
+
+export default About
